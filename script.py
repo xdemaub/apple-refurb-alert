79e7aa87e09b49dd138a71d99b0c71fa99d86d93
xdemaub <xdemaub@yahoo.fr>
2026-05-21 13:52:08 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,24 @@
 import requests
 import os
 
+URL = "https://www.apple.com/fr/shop/refurbished/mac"
+
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 
-requests.post(
-    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-    data={
-        "chat_id": CHAT_ID,
-        "text": " BOT OK (test final)"
-    }
-)
+def send(msg):
+    requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        data={"chat_id": CHAT_ID, "text": msg}
+    )
 
-print("message envoyé")
+def check():
+    response = requests.get(URL)
+    
+    if "M5" in response.text:
+        send(" MacBook M5 détecté sur Apple Refurb !\n https://www.apple.com/fr/shop/refurbished/mac")
+        print("M5 trouvé")
+    else:
+        print("Rien trouvé")
+
+check()
