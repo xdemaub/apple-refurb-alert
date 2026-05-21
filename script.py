57c42522c02335540bd42e4c16bc7bb8462ede46
xdemaub <xdemaub@yahoo.fr>
2026-05-21 12:47:24 +0200

    Add script to check Mac availability and notify

---
+++ script.py
@@ -0,0 +1,17 @@
+import requests
+
+TOKEN = "test"
+CHAT_ID = "test"
+
+URL = "https://www.apple.com/fr/shop/refurbished/mac"
+
+def send(msg):
+    requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+                  data={"chat_id": CHAT_ID, "text": msg})
+
+def check():
+    r = requests.get(URL)
+    if "M5" in r.text:
+        send("🚨 Un Mac M5 est dispo !")
+
+check()
