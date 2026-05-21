a9b31a62503ac0e7be1c71853cbc26fb0a67671b
xdemaub <xdemaub@yahoo.fr>
2026-05-21 13:38:56 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,15 @@
-print("✅ CA MARCHE PARFAITEMENT 🚀")
+import requests
+import os
+
+TOKEN = os.environ.get("TELEGRAM_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": " TON BOT MARCHE "
+})
+
+print("Message envoyé")
+``
